838795225cf5c56d047651e4369f83855a9153a7
Lincoln Simmons <lincolnsimmons@gmail.com>
2019-07-12 09:56:42 -0700

    33_tickers.go

---
+++ 33_tickers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	}()
+
+	time.Sleep(100 * time.Second)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+}

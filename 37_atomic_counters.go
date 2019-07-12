84cb61ed9436c44fb86f7fd75fcf09c9d6d3189a
Lincoln Simmons <lincolnsimmons@gmail.com>
2019-07-12 11:43:56 -0700

    37_atomic_counters.go

---
+++ 37_atomic_counters.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	var ops uint64
+
+	for i := 0; i < 50; i++ {
+		// This will dispatch 50 workers each monotonically
+		// incrementing the single uint64 counter reference
+		go func() {
+			for {
+				atomic.AddUint64(&ops, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	opsFinal := atomic.LoadUint64(&ops)
+	fmt.Println("ops:", opsFinal)
+}

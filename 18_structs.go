0f997f72e5a2c4da7722b9d66061c2ebc7fb51e7
Lincoln Simmons <lincolnsimmons@gmail.com>
2019-07-11 20:59:55 -0700

    18_structs.go

---
+++ 18_structs.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+type person struct {
+	name string
+	age int
+}
+
+func main() {
+	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name: "Alicia", age: 20})
+	fmt.Println(person{name: "Ol' Greg"})
+	fmt.Println(&person{name: "Lincoln", age: 32})
+
+	s := person{name: "Sean", age: 5000}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+	sp.age = 51
+	fmt.Println(sp.age)
+}

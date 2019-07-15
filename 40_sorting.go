e26433766d1ebab744bf81e2607d5628db76ce64
Lincoln Simmons <lincolnsimmons@gmail.com>
2019-07-14 22:25:34 -0700

    40_sorting.go

---
+++ 40_sorting.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{7, 3, 2, 8, 5}
+	sort.Ints(ints)
+	fmt.Println("Ints:", ints)
+
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted:", s)
+}

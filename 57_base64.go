395dd0c51248bac16f2325bddbcf2dc88178515f
Lincoln Simmons <lincolnsimmons@gmail.com>
2019-11-09 23:07:32 -0700

    57_base64.go

---
+++ 57_base64.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	data := "acb123!*#&#asdf'.%&329_#~?><*%&@*&~)$>?"
+
+	sEnc := base64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
+	sDec, _ := base64.StdEncoding.DecodeString(sEnc)
+	fmt.Println(string(sDec))
+
+	/* There's also URL compatible encoding */
+	uEnc := base64.URLEncoding.EncodeToString([]byte(data))
+	fmt.Println(uEnc)
+	uDec, _ := base64.URLEncoding.DecodeString(uEnc)
+	fmt.Println(string(uDec))
+}
